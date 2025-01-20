5f2ee7068f5a3acb8c0e366bb2b38b493d2231cb
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2025-01-20 17:07:35 +0100

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
-"""Change making module.
-"""
+
+"""Making change"""
 
 
 def makeChange(coins, total):
+    """
+    Given an array of coins and a total amount, return the minimum number
+    of coins needed to make up that amount.
+    :param coins: list of integers
+    :param total: integer
+    :return: integer
+    """
+    if not coins or coins is None:
+        return -1
     if total <= 0:
         return 0
-
-    # Create an array to store the minimum number of coins required to reach each value
-    min_coins = [float('inf')] * (total + 1)
-    min_coins[0] = 0
-
+    change = 0
+    coins = sorted(coins)[::-1]
     for coin in coins:
-        for i in range(coin, total + 1):
-            # Update the minimum number of coins required for each value
-            min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
-
-    # Return the minimum number of coins required to reach the total value
-    return min_coins[total] if min_coins[total] != float('inf') else -1
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
